ac151a70445f5a91405937de0f65482ee1fd540f
Sontara444 <sontararajput@gmail.com>
2023-02-20 20:42:21 +0530

    calculator

---
+++ app.js
@@ -0,0 +1,10 @@
+
+// console.log("jai shree ram")
+// const vanillaTilt = document.querySelector('.vanilla-tilt');
+// console.log(vanillaTilt)
+
+// vanillaTilt.isContentEditable(document.querySelectorAll(".container"),{
+//     max: 25,
+//     speed : 400,
+
+// })
